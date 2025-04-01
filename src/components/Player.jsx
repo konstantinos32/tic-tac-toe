1b5eb6eb1303c9a118eadecc4a11dd80df719898
konstantinos32 <76957273+konstantinos32@users.noreply.github.com>
2025-04-01 11:42:15 +0200

    start of a working edit name button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,26 @@
-export default function Player({name, symbol}) {
-return (
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+
+  const [ isEditing, setIsEditing ] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+
+  return (
     <li>
-          <span className="player">
-          <span className="player-name">{name}</span>
+      <span className="player">
+        {playerName}
         <span className="player-symbol">{symbol}</span>
-          </span>
-          <button>Edit</button>
-        </li>
-        
-);
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
 }
