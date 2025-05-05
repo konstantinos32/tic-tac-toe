aca93b72715707a80fbcb14bfb18a2da21922279
konstantinos32 <76957273+konstantinos32@users.noreply.github.com>
2025-05-05 12:04:54 +0300

    player symbols now change

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -14,9 +14,11 @@ export default function GameBoard() {
       const updatedBoard = [
         ...prevGameBoard.map((innenArray) => [...innenArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X"; // or 'O' depending on the player
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol; // or 'O' depending on the player
       return updatedBoard;
     });
+
+    onSelectSquare();
   }
 
   return (
